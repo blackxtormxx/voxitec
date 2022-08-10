b00238409ed770971307cd2f6bf1f57ee40f6cea
blackxtormxx <it20061166@my.sliit.lk>
2022-08-10 09:15:51 +0530

    1

---
+++ src/App.js
@@ -0,0 +1,86 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="rectangle">
+            <div className='frame148'>
+              <div className='yourwork'><h5>your work summary</h5></div>
+              <div className='nov'><p> Nov - July</p></div>
+            </div>
+        <div className="frame145">
+
+          <div className='frame110'>
+            <div className='status'>
+              <div className='statusdot1'></div> 
+              <div className='statusfont1'>Last Month</div>
+            </div>
+            <div className='status2'>
+              <div className='statusdot2'></div> 
+              <div className='statusfont2'>This Month</div>
+            </div>
+          </div>
+          <div className='secondary'>          
+            <div className='button1'>
+              <div className='button1a'>
+              <div className='buttontext'>
+                Daily
+              </div>
+              </div>
+            </div>
+            <div className='button2'>
+              <div className='button2a'>
+              <div className='buttontext2'>
+                Monthly
+              </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+              <div className="group66">
+              <div className='text4'>600</div>
+              <div className='line'></div>
+
+              <div className='text3'>400</div>
+              <div className='line1'></div>
+
+              <div className='text2'>200</div>
+              <div className='line2'></div>
+
+              <div className='text1'>0</div>
+              <div className='line3'></div>
+              </div>
+
+        <div className="card">
+        <div className='frame146'>
+        <div className='status'>
+              <div className='statusdot1'></div> 
+              <div className='statusfont1'>Last Month</div>
+            </div>
+            <div className='influsion'>
+            <div className='influsiontext'>&#x2191;2.36%</div>
+            </div>
+        </div>
+        <div className='frame147'>
+        <div className='status2'>
+              <div className='statusdot2'></div> 
+              <div className='statusfont2'>This Month 
+</div>
+            </div>
+
+            <div className='influsion2'>
+            <div className='influsiontext2'>&#x2193;2.36%</div>
+            </div>
+        </div>
+
+        </div>
+
+
+        <div className='bottomtext1'><h3>9845</h3></div>
+        <div className='xbadge'><div className='xbadgetext'>&#x2191;829</div></div>
+        <div className='signup'><p>Sign-Ups past 30 days</p></div>
+    </div>
+  );
+}
+
+export default App;
